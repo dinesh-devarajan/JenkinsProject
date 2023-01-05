733f5b7a62ea3e17708d435e7d8816e5c2aa2f0e
dinesh-devarajan <122002420+dinesh-devarajan@users.noreply.github.com>
2023-01-05 16:27:31 +0530

    Update StringPractice.java

--- StringPractice.java
+++ StringPractice.java
@@ -1,21 +1,8 @@
 public class StringPractice {
 	public static void main(String[] args) {
-		String s ="Hello World";
-		String s1 = "  hello world  ";
 		
-				int length = s.length();
-				System.out.println(length);
-		
-				String upperCase = s.toUpperCase();
-				System.out.println(upperCase);
-				
-				String lowerCase = s.toLowerCase();
-				System.out.println(lowerCase);
-				
-				boolean equals = s.equals(s1);
-				System.out.println(equals);
 		System.out.println("Hi");
-		System.out.println("Hiiii");
+		
 		
 				
 			
